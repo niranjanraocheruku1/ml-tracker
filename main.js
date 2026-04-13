cd9db840dee778c20965d55da3f714a45060d322
Niranjan Rao Cheruku <niranjanraocheruku1@gmail.com>
2026-04-13 15:14:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    webPreferences: { nodeIntegration: false },
+    title: 'ML Tracker'
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
